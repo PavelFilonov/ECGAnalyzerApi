c249f6c7da1a6d24582dc0f1e41d9a473b9894ed
Pavel Filonov <qqq.qqq48@yandex.ru>
2023-05-06 00:31:00 +0300

    Initial commit

---
+++ src/test/java/ru/vsu/csf/ecganalyzerapi/EcgAnalyzerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.csf.ecganalyzerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcgAnalyzerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
