16e3008d4c132f93ac34360671a5a5db743ae2f2
Pavel Filonov <qqq.qqq48@yandex.ru>
2023-05-15 06:38:26 +0300

    Структура идентификации стадий

---
+++ src/main/java/ru/vsu/csf/ecganalyzerapi/analyzer/identifier/CicatriceStageIdentifier.java
@@ -0,0 +1,15 @@
+package ru.vsu.csf.ecganalyzerapi.analyzer.identifier;
+
+import org.springframework.stereotype.Service;
+import ru.vsu.csf.ecganalyzerapi.entity.File;
+import ru.vsu.csf.ecganalyzerapi.entity.ReportPeriod;
+
+import java.util.List;
+
+@Service
+public class CicatriceStageIdentifier implements Identifier {
+    @Override
+    public List<ReportPeriod> identify(File file) {
+        return null;
+    }
+}
